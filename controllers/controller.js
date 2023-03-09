28eba247d9f0fa9e062482530585b906785a59ea
ibra101408 <danikvod@gmail.com>
2023-03-09 07:28:35 +0200

    [#184605393]

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,34 @@
 
+
 const express = require('express');
 const router = express.Router();
 const model = require('../models/model');
+
+// GET all to-do items
+router.get('/', async (req, res) => {
+    try {
+        const todos = await model.getAll();
+        res.render('index', { todos });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
+
+//delete todo
+/*
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await model.delete(id);
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});*/
 
 
 module.exports = router;
--- models/model.js
+++ models/model.js
@@ -1,3 +1,32 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('identifier.sqlite')
+
+
+// Export functions to interact with the database
+module.exports = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            db.all('SELECT * FROM todos', [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }/*
+    delete: (id) => {
+        return new Promise((resolve, reject) => {
+            db.run('DELETE FROM todos WHERE id = ?', [id], function (err) {
+                if (err) {
+                    reject(err);
+                } else if (this.changes === 0) {
+                    reject(new Error(`No todo with id ${id} found`));
+                } else {
+                    resolve(this.changes);
+                }
+            });
+        })
+    };*/
+};
